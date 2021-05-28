0daa020e7cf89e179702dabdad8043bd94c2a9b0
B LAC JASS <73884202+JASS697@users.noreply.github.com>
2021-05-29 02:16:34 +0530

    Update start.py

--- start.py
+++ start.py
@@ -1,6 +1,6 @@
-from os import system # chala ja bhai
-system ("git clone https://GitHub.com/JASS697/BLAC-2.0-USERBOT- && cd && python3 -m ULTRA")
+ from os import system # chala ja bhosdike madarchod
+repo = "https://github.com/JASS697/BLAC-2.0-USERBOT"
+main = "userbot"
+system ("git clone {} && cd BLAC-2.0-USERBOT && python3 -m {}".format(repo, main))
 # try af
-print("OH SYSTEM IS CRASHED")
-system("python3 -m ULTRA")
-print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO @BLACUSERBOT_PUBLIC")
+print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO SUPPORT GROUP")
