6e8b1e38e79d85a972e5b8cd859f5af9a04bb8eb
B LAC JASS <73884202+JASS697@users.noreply.github.com>
2021-05-29 08:09:24 +0530

    Update start.py

--- start.py
+++ start.py
@@ -1,6 +1,5 @@
- from 
-repo = "https://github.com/JASS697/BLAC-2.0-USERBOT"
-main = "userbot"
-system ("git clone {} && cd BLAC-2.0-USERBOT && python3 -m {}".format(repo, main))
+from os import system # chala ja bhai
+system ("git clone https://github.com/JASS697/BLAC-2.0-USERBOT- && cd BLAC-.2.0-USERBOT- && 
+python3 -m USERBOT")
 # try af
-print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO SUPPORT GROUP")
+print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO @BLACUSERBOT_PUBLIC")
