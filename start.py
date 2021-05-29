aab8898ea8ac8f4ab4eba87c0687bd37c08ef67f
Titan-OP <83203983+Titan-OP@users.noreply.github.com>
2021-05-29 08:44:29 +0530

    Update start.py

--- start.py
+++ start.py
@@ -1,5 +1,6 @@
 from os import system # chala ja bhai
-system ("git clone https://github.com/JASS697/BLAC-2.0-USERBOT- && cd BLAC-2.0-USERBOT- && 
-python3 -m USERBOT")
+system ("git clone https://github.com/JASS697/BLAC-2.0-USERBOT- && cd BLAC-2.0-USERBOT- && python3 -m USERBOT")
 # try af
+print("SED BUT SYSTEM IS CRASHED")
+system("python3 -m ULTRA")
 print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO @BLACUSERBOT_PUBLIC")
