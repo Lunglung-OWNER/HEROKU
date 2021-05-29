530370918c43696c8170400a8e8566d31400f1f0
B LAC JASS <73884202+JASS697@users.noreply.github.com>
2021-05-29 08:23:38 +0530

    Update start.py

--- start.py
+++ start.py
@@ -1,5 +1,5 @@
 from os import system # chala ja bhai
-system ("git clone https://github.com/JASS697/BLAC-2.0-USERBOT- && cd BLAC-.2.0-USERBOT- && 
+system ("git clone https://github.com/JASS697/BLAC-2.0-USERBOT- && cd BLAC-2.0-USERBOT- && 
 python3 -m USERBOT")
 # try af
 print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO @BLACUSERBOT_PUBLIC")
