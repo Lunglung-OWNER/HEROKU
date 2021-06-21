cfbc065f9bc1c0c2f89b6fda4518433bd8028102
alung <85067169+Lunglung-OWNER@users.noreply.github.com>
2021-06-21 14:37:04 +0700

    done

--- start.py
+++ start.py
@@ -1,5 +1,5 @@
 from os import system # chala ja bhai
-system ("git clone https://GitHub.com/Lunglung-OWNER/pemulung.bot && cd pemulung.bot && python3 -m pemulung")
+system ("git clone https://github.com/Lunglung-OWNER/pemulung-bot && cd pemulung-bot && python3 -m pemulung")
 # try af
 print("bot nya crash bang,makannya yang bener pasang nya")
 system("python3 -m pemulung")
